2b94692dcdb1033a0eecc7aad158d8d0bc829366
new92 <94779840+new92@users.noreply.github.com>
2023-02-25 22:53:33 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float num1, num2, result;
+    char op;
+
+    cout << "Please enter the first number: ";
+    cin >> num1;
+    cout << "Please enter the operator (+, -, *, /): ";
+    cin >> op;
+    cout << "Please enter the second number: ";
+    cin >> num2;
+
+    switch(op) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+            cout << "Invalid operator !";
+            return 0;
+    }
+
+    cout << num1 << " " << op << " " << num2 << " = " << result;
+
+    return 0;
+}
